a11c3d356d1ede9e6d090a176411b7a2a01fc38e
Szymon A <szymon.anikiej@gmail.com>
2021-12-21 01:42:27 +0100

    Updated index.html

--- CoinbaseBot/views.py
+++ CoinbaseBot/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from CoinbaseBot.CoinBaseConnector import CoinBaseConnector
+
 
 def index(request):
+    conn = CoinBaseConnector()
+    price = conn.getProductPrice('BTC-USD')
     template = loader.get_template('CoinbaseBot/index.html')
-    context = dict()
+    context = {'price': price}
     return HttpResponse(template.render(context, request))
