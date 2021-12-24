ab4cdc6d75025cc30f9ce0a546127b3564eb2e7e
Szymon Anikiej <szymon@anikiej.com>
2021-12-24 21:24:09 +0100

    Added ajax request for updating price in user's view

--- CoinbaseBot/views.py
+++ CoinbaseBot/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from CoinbaseBot.CoinBaseConnector import CoinBaseConnector
 
 
 def index(request):
+    template = loader.get_template('CoinbaseBot/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def getPrice(request):
     conn = CoinBaseConnector()
     price = conn.getProductPrice('BTC-USD')
-    template = loader.get_template('CoinbaseBot/index.html')
     context = {'price': price}
-    return HttpResponse(template.render(context, request))
+    return JsonResponse(context)
